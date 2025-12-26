a3f85f7e4c0ca3bb16646d69dc8f8b407a504932
Valentin Fontana <valenfontana7@gmail.com>
2025-12-26 00:15:58 -0300

    chore(prisma): add prisma.config.js for runtime in production

---
+++ prisma.config.js
@@ -0,0 +1,11 @@
+const { defineConfig, env } = require('prisma/config');
+
+module.exports = defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+});
